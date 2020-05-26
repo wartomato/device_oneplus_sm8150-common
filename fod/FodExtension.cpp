d4429cfbec7c5f6060c0611ae23aa6c71e105ed2
Michele Bono <bono.michele94@gmail.com>
2020-05-26 22:40:36 +0200

    sm8150-common: Swap z values

--- fod/FodExtension.cpp
+++ fod/FodExtension.cpp
@@ -17,7 +17,7 @@
 #include <compositionengine/FodExtension.h>
 
 uint32_t getFodZOrder(uint32_t, bool touched) {
-    return touched ? 0xfc8 : 0xbd6;
+    return touched ? 0xbd6 : 0xfc8;
 }
 
 uint32_t getFodUsageBits(uint32_t usageBits, bool) {
