LineageOS/android_device_oneplus_sm8150-common

sm8150-common: gpt-utils: Remove dynamic partitions from AB_PTN_LIST if needed
Change-Id: I806e039a27510b69d512ff7de374925f0e308316

soong/bootctrl.go
@@ -1,0 +1,40 @@
+package msmnile
+
+import (
+    "android/soong/android"
+    "android/soong/cc"
+    "strings"
+)
+
+func bootctrlFlags(ctx android.BaseContext) []string {
+    var cflags []string
+
+    var deviceName = ctx.Config().DeviceName()
+
+    if strings.HasPrefix(deviceName, "hotdog") {
+        cflags = append(cflags, "-DUSE_DYNAMIC_PARTITIONS")
+    }
+    return cflags
+}
+
+func bootctrlLibrary(ctx android.LoadHookContext) {
+    type props struct {
+        Target struct {
+            Android struct {
+                Cflags []string
+            }
+        }
+    }
+
+    p := &props{}
+    p.Target.Android.Cflags = bootctrlFlags(ctx)
+    ctx.AppendProperties(p)
+}
+
+func bootctrlLibraryFactory() android.Module {
+    module, library := cc.NewLibrary(android.HostAndDeviceSupported)
+    library.BuildOnlyStatic()
+    newMod := module.Init()
+    android.AddLoadHook(newMod, bootctrlLibrary)
+    return newMod
+}

wartomato/device_oneplus_sm8150-common

soong/bootctrl.go
@@ -1,0 +1,39 @@
+package msmnile
+
+import (
+    "android/soong/android"
+    "android/soong/cc"
+    "strings"
+)
+
+func bootctrlFlags(ctx android.BaseContext) []string {
+    var cflags []string
+
+    var deviceName = ctx.Config().DeviceName()
+
+    if strings.HasPrefix(deviceName, "hotdog") {
+        cflags = append(cflags, "-DUSE_DYNAMIC_PARTITIONS")
+    }
+    return cflags
+}
+
+func bootctrlLibrary(ctx android.LoadHookContext) {
+    type props struct {
+        Target struct {
+            Android struct {
+                Cflags []string
+            }
+        }
+    }
+
+    p := &props{}
+    p.Target.Android.Cflags = bootctrlFlags(ctx)
+    ctx.AppendProperties(p)
+}
+
+func bootctrlLibraryFactory() android.Module {
+    module, _ := cc.NewLibrary(android.DeviceSupported)
+    newMod := module.Init()
+    android.AddLoadHook(newMod, bootctrlLibrary)
+    return newMod
+}