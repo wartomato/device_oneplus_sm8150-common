fcbfe960effae0a5ca006428ba553d6cb2f82ada
Ralf Luther <luther.ralf@gmail.com>
2020-09-06 15:50:19 +0200

    Revert "sm8150-common [WIP]: update dax default values"
    This reverts commit 0b46ca19f98cc666bac0bf8118e17271ee04cb5e.

--- common.mk
+++ common.mk
@@ -142,9 +142,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/product,system/product)
-
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl:64 \
