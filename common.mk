f440e1091315b85e9b04f8e554163c36cc114122
Ralf Luther <luther.ralf@gmail.com>
2020-09-26 16:12:52 +0200

    Revert "sm8150-common [WIP]: add custom small bootanimation"
    This reverts commit 4325b13b0902fa3573afe6be416ce3ab40f9b7c0.

--- common.mk
+++ common.mk
@@ -142,10 +142,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
-# Bootanimation
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/bootanimation.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
-
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl:64 \
