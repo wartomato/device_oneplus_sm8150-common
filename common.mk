bc0dbdc131540110b6841652a8ed87a00528edc7
Ralf Luther <luther.ralf@gmail.com>
2023-01-01 18:10:10 +0100

    sm8150-common: [test] add overlay path to .mk

--- common.mk
+++ common.mk
@@ -310,6 +310,7 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-aicp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
