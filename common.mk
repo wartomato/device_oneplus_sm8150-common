8d023e80c78199684e251fc57a12c1285d1b73de
Ralf Luther <luther.ralf@gmail.com>
2023-02-12 19:13:02 +0100

    sm8150-common: [fixup] delete unneeded overlay path

--- common.mk
+++ common.mk
@@ -307,7 +307,6 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-aicp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
