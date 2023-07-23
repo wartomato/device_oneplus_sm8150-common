5019faf5fbacedf9d21556b38135e21d1847a00c
Ralf Luther <luther.ralf@gmail.com>
2023-07-23 13:53:44 +0200

    sm8150-common: overlay: Compact cached app heaps in the background
     Background ART heap compaction for cached apps has been
     recommended for Android Go devices for a while, and Google has enabled it for
     the Pixel 5 on RQ1A.201205.010. This feature should help fit more cached apps
     in memory
    
    Change-Id: I544927bec35109b6e41e2b1836b283e15025a4e1

--- common.mk
+++ common.mk
@@ -315,7 +315,8 @@ PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
     OPlusFrameworksResCommon \
-    OPlusSystemUIResCommon
+    OPlusSystemUIResCommon \
+    SimpleDeviceConfigCommon
 
 # Power
 PRODUCT_PACKAGES += \
